36e21cb1485e42b01271283d6f407d8215bb0e86
Mr. Dalien <daniel.stuardo@gmail.com>
2023-02-06 16:32:51 -0300

    Add files via upload

--- tests/pacman.sh
+++ tests/pacman.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rxvt -j -g 75x103 -fn "xft:FantasqueSansMono-Regular:pixelsize=5" -e ./tests/pacman $1 $2 $3 $4
+#rxvt -lsp 0 -j -g 75x103 -pixmap "accion_osada.png" -fn "xft:FantasqueSansMono-Regular:pixelsize=5" -e ./tests/pacman $1 $2 $3 $4
+rxvt -lsp 0 +sb -b 0 -w 0 -j -g 75x103 -fn "xft:FantasqueSansMono-Regular:pixelsize=5" -e ./tests/pacman $1 $2 $3 $4
 
 
